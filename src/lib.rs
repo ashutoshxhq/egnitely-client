d9f5e49788d482833eb9f464f1516fa1334be3a5
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-05-15 10:56:54 +0530

    added more macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,71 @@
 #[macro_export]
-macro_rules! egnitely_fn {
+macro_rules! register_egnitely_fn {
     ($func_name:ident) => {
         #[no_mangle]
         pub extern "C" fn handler(data_pointer: *mut c_char) -> *mut c_char {
+            let data = unsafe { CString::from_raw(data_pointer) };
+            let data_str = data.into_string().unwrap();
+            println!("Recieved In Handler: {}", data_str);
+
+            let json_data: Value = serde_json::from_str(data_str.as_str()).unwrap();
+
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let response = rt.block_on(async { $func_name(json_data).await });
+
+            println!("Returning form Handler: {}", data_str);
+            let result_cstr = CString::new(response.to_string()).unwrap();
+            result_cstr.into_raw()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! register_egnitely_post_install_fn {
+    ($func_name:ident) => {
+        #[no_mangle]
+        pub extern "C" fn post_install(data_pointer: *mut c_char) -> *mut c_char {
+            let data = unsafe { CString::from_raw(data_pointer) };
+            let data_str = data.into_string().unwrap();
+            println!("Recieved In Handler: {}", data_str);
+
+            let json_data: Value = serde_json::from_str(data_str.as_str()).unwrap();
+
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let response = rt.block_on(async { $func_name(json_data).await });
+
+            println!("Returning form Handler: {}", data_str);
+            let result_cstr = CString::new(response.to_string()).unwrap();
+            result_cstr.into_raw()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! register_egnitely_pre_trigger_fn {
+    ($func_name:ident) => {
+        #[no_mangle]
+        pub extern "C" fn pre_trigger(data_pointer: *mut c_char) -> *mut c_char {
+            let data = unsafe { CString::from_raw(data_pointer) };
+            let data_str = data.into_string().unwrap();
+            println!("Recieved In Handler: {}", data_str);
+
+            let json_data: Value = serde_json::from_str(data_str.as_str()).unwrap();
+
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let response = rt.block_on(async { $func_name(json_data).await });
+
+            println!("Returning form Handler: {}", data_str);
+            let result_cstr = CString::new(response.to_string()).unwrap();
+            result_cstr.into_raw()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! register_egnitely_post_trigger_fn {
+    ($func_name:ident) => {
+        #[no_mangle]
+        pub extern "C" fn post_trigger(data_pointer: *mut c_char) -> *mut c_char {
             let data = unsafe { CString::from_raw(data_pointer) };
             let data_str = data.into_string().unwrap();
             println!("Recieved In Handler: {}", data_str);
