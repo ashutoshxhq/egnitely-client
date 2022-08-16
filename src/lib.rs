c54768d5c53b5b0b6de67f9c3a97431db5560441
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-08-16 16:33:59 +0530

    added context

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 use std::error::Error;
+
+pub struct Context {
+    name: String, 
+    version: String
+}
 pub trait EgnitelyContext {
     fn get_function_name(&mut self) -> Result<String, Box<dyn Error>>;
     fn get_version(&mut self) -> Result<String, Box<dyn Error>>;
