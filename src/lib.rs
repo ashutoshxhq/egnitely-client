9734599a879c49ab0c5675ba4603798028b017fd
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-08-16 16:23:06 +0530

    added context trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-pub struct EgnitelyContext {
-    name: String,
-    version: String,
+use std::error::Error;
+pub trait EgnitelyContext {
+    fn get_function_name(&mut self) -> Result<String, Box<dyn Error>>;
+    fn get_version(&mut self) -> Result<String, Box<dyn Error>>;
 }
