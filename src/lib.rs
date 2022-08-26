effd83778dc33989b58673d6fef52dd14657356b
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-08-26 15:42:17 +0530

    updated egnitely client

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,38 @@
-use std::error::Error;
+use aws_config::SdkConfig;
+use serde_json::Value;
 
-pub struct Context {
-    pub name: String, 
-    pub version: String
+pub struct RequestContext {
+    template_name: String,
+    template_version: String,
+    aws_sdk_config: SdkConfig,
+    data: Value,
+    headers: Value,
 }
-pub trait EgnitelyContext {
-    fn get_function_name(&mut self) -> Result<String, Box<dyn Error>>;
-    fn get_version(&mut self) -> Result<String, Box<dyn Error>>;
+
+impl RequestContext {
+    pub fn new(template_name: String, template_version: String, aws_sdk_config: SdkConfig, data: Value, headers: Value) -> Self {
+        Self {
+            template_name,
+            template_version,
+            aws_sdk_config,
+            data,
+            headers,
+        }
+    }
+
+    pub fn template_name(&mut self) -> String {
+        self.template_name.clone()
+    }
+    pub fn template_version(&mut self) -> String {
+        self.template_version.clone()
+    }
+    pub fn aws_sdk_config(&mut self) -> SdkConfig {
+        self.aws_sdk_config.clone()
+    }
+    pub fn data(&mut self) -> Value {
+        self.data.clone()
+    }
+    pub fn headers(&mut self) -> Value {
+        self.headers.clone()
+    }
 }
